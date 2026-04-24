fe177e6d06e8f0d09c4fccaeec89399ccf53ab5e
Alvaro <alvher.orellana@gmail.com>
2026-04-24 12:18:23 -0400

    formatting

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -9,112 +9,59 @@ from typing import Callable, Iterable
 #
 # Implementation of a prelude of elementary functions.
 
+def mul(x: float, y: float) -> float: return x * y
 
-def mul(x: float, y: float) -> float:
-    "$f(x, y) = x * y$"
-    return x * y
+def id(x: float) -> float: return x
 
+def add(x: float, y: float) -> float: return x + y
 
-def id(x: float) -> float:
-    "$f(x) = x$"
-    return x
+def neg(x: float) -> float: return -x
 
+def lt(x: float, y: float) -> float: return 1.0 if x < y else 0.0
 
-def add(x: float, y: float) -> float:
-    "$f(x, y) = x + y$"
-    return x + y
+def eq(x: float, y: float) -> float: return 1.0 if x == y else 0.0
 
+def max(x: float, y: float) -> float: return x if x > y else y
 
-def neg(x: float) -> float:
-    "$f(x) = -x$"
-    return -x
-
-
-def lt(x: float, y: float) -> float:
-    "$f(x) =$ 1.0 if x is less than y else 0.0"
-    return 1.0 if x < y else 0.0
-
-
-def eq(x: float, y: float) -> float:
-    "$f(x) =$ 1.0 if x is equal to y else 0.0"
-    return 1.0 if x == y else 0.0
-
-
-def max(x: float, y: float) -> float:
-    "$f(x) =$ x if x is greater than y else y"
-    return x if x > y else y
-
-
-def is_close(x: float, y: float) -> float:
-    "$f(x) = |x - y| < 1e-2$"
-    return 1.0 if abs(x - y) < 1e-2 else 0.0
-
+def is_close(x: float, y: float) -> float: return 1.0 if abs(x - y) < 1e-2 else 0.0
 
 def sigmoid(x: float) -> float:
-    r"""
-    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
-    (See https://en.wikipedia.org/wiki/Sigmoid_function )
-    Calculate as
-    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
-    for stability.    """
     if x >= 0:
         return 1.0 / (1.0 + exp(-x))
     else:
         return 1.0 / (1.0 + exp(x))
 
-
-def relu(x: float) -> float:
-    """
-    $f(x) =$ x if x is greater than 0, else 0
-    (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)    """
-    return max(0.0, x)
-
+def relu(x: float) -> float: return max(0.0, x)
 
 EPS = 1e-6
 
+def log(x: float) -> float: return math.log(x + EPS)
 
-def log(x: float) -> float:
-    "$f(x) = log(x)$"
-    return math.log(x + EPS)
+def exp(x: float) -> float: return math.exp(x)
 
+def log_back(x: float, d: float) -> float: return d * (1 / x)
 
-def exp(x: float) -> float:
-    "$f(x) = e^{x}$"
-    return math.exp(x)
-
-
-def log_back(x: float, d: float) -> float:
-    r"If $f = log$ as above, compute $d \times f'(x)$"
-    return d * (1 / x)
-
-def inv(x: float) -> float:
-    "$f(x) = 1/x$"
-    return 1.0 / x
-
+def inv(x: float) -> float: return 1.0 / x
 
 def inv_back(x: float, d: float) -> float:
-    r"If $f(x) = 1/x$ compute $d \times f'(x)$"
     inv_derivative = -1 / x**2
     return d * inv_derivative
 
-
 def relu_back(x: float, d: float) -> float:
-    r"If $f = relu$ compute $d \times f'(x)$"
     relu_derivative = 1 if x > 0 else 0
     return d * relu_derivative
-
 
 # ## Task 0.3
 
 # Small practice library of elementary higher-order functions.
-
 
 def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
     """  
     Higher-order map.  
     See https://en.wikipedia.org/wiki/Map_(higher-order_function)  
     Args:        fn: Function from one value to one value.  
-    Returns:         A function that takes a list, applies `fn` to each element, and returns a         new list    """    # TODO: Implement for Task 0.3.
+    Returns:         A function that takes a list, applies `fn` to each element, and returns a         new list    """
+    # TODO: Implement for   3.
     raise NotImplementedError('Need to implement for Task 0.3')
 
 
